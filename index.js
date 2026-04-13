a0954e0693581f49558234f42c9c22bd48df2b73
anish-singh-rawat <www.computernetwork@gmail.com>
2026-04-13 15:42:49 +0530

    feat : allow user to connect APi with 75 localhost port

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   "https://www.visualeyeye.netlify.app",
   "http://localhost:5173",
   "http://localhost:5174",
+  "http://localhost:5175",
   "http://139.59.65.108",
 ];
 
